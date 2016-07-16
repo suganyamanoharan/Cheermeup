041183559a19d4e29204bca753c72dc742932ac4
Constance Noziere <cnoziere@cnoziere-mn1.linkedin.biz>
2016-07-16 01:44:46 -0700

    wmyh: moved to a separate file

---
+++ wmyh.py
@@ -0,0 +1,34 @@
+from flask import render_template, request, jsonify
+import logging
+from flask import Flask, redirect, url_for
+
+app= Flask(__name__)
+"""app = create_app(__name__)"""
+app.debug = True
+logger = logging.getLogger(__name__)
+
+topics = ["food", "landscapes", "tv series", "cats", "dogs", "pokemon", "travel", "technology", "sports",
+              "funny gifs", "music", "hatching chicks", "outer space"]
+selected = []
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return render_template('hello.html')
+
+@app.route('/wmyh', methods=['GET', 'POST'])        # what makes you happy?
+def wmyh():
+    if request.method == 'POST':
+        if 'done' in request.form.keys():
+            selected_topics = []
+            for item in request.form.keys():
+                if item != 'done':
+                    ## put in the DB
+                    selected_topics.append(item)
+            return redirect(url_for('hello'))
+        else:
+            new_topic = request.form.get('search')
+            topics.append(new_topic)
+            selected.append(new_topic)
+            return render_template('wmyh.html', topics=topics, selected=selected)
+
+    return render_template('wmyh.html', topics=topics)
