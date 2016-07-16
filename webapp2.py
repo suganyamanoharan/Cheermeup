80bb31ff6f2299d3862d55ad089a703f89698bbc
Bhavika Devnani <bdevnani@bdevnani-mn1.linkedin.biz>
2016-07-16 03:38:50 -0700

    Images repo

--- webapp2.py
+++ webapp2.py
@@ -8,6 +8,7 @@ app= Flask(__name__)
 app.debug = True
 logger = logging.getLogger(__name__)
 
+
 @app.route('/imgs')
 def generatePaths(imlist=imageGenerator.weighted_choice(selected_tags)):
     return render_template('images.html', image_list= imlist)
