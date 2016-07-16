58a6f88e1be1b273694740b7eaba0a17c6ae9160
Constance Noziere <cnoziere@cnoziere-mn1.linkedin.biz>
2016-07-15 23:29:24 -0700

    Added checkboxes for topics

--- webapp2.py
+++ webapp2.py
@@ -2,17 +2,28 @@ from flask import render_template, request, jsonify
 import logging
 from flask import Flask
 
+import sys
 
 app= Flask(__name__)
 """app = create_app(__name__)"""
 app.debug = True
 logger = logging.getLogger(__name__)
 
-
+def selected_topics(topics):
+    print topics
 
 @app.route('/home/<name>')
-def hello(name="Default"):
-    return render_template('abcd.html', name=name )
+def home(name="Default"):
+    return render_template('abcd.html', name=name)
+
+@app.route('/wmyh', methods=['GET', 'POST'])        # what makes you happy?
+def wmyh():
+    topics = ["food", "landscapes", "tv series", "cats", "dogs", "pokemon", "travel", "technology", "sports",
+            "funny gifs", "music", "hatching chicks", "outer space"]
+    if request.method == 'POST':
+        selected_topics(request.form)
+    elif request.method == 'GET':
+        return render_template('wmyh.html', topics=topics)
 
 
 
