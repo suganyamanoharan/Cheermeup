eb891c3e875377364b7fcf138bcb1892f4776cd8
Constance Noziere <cnoziere@cnoziere-mn1.linkedin.biz>
2016-07-16 01:34:21 -0700

    wymh: added search bar

--- webapp2.py
+++ webapp2.py
@@ -1,16 +1,20 @@
 from flask import render_template, request, jsonify
 import logging
-from flask import Flask
-
-import sys
+from flask import Flask, redirect, url_for
 
 app= Flask(__name__)
 """app = create_app(__name__)"""
 app.debug = True
 logger = logging.getLogger(__name__)
 
-def selected_topics(topics):
-    print topics
+
+topics = ["food", "landscapes", "tv series", "cats", "dogs", "pokemon", "travel", "technology", "sports",
+              "funny gifs", "music", "hatching chicks", "outer space"]
+selected = []
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return render_template('hello.html')
 
 @app.route('/home/<name>')
 def home(name="Default"):
@@ -18,12 +22,18 @@ def home(name="Default"):
 
 @app.route('/wmyh', methods=['GET', 'POST'])        # what makes you happy?
 def wmyh():
-    topics = ["food", "landscapes", "tv series", "cats", "dogs", "pokemon", "travel", "technology", "sports",
-            "funny gifs", "music", "hatching chicks", "outer space"]
     if request.method == 'POST':
-        selected_topics(request.form)
-    elif request.method == 'GET':
-        return render_template('wmyh.html', topics=topics)
+        if 'done' in request.form.keys():
+            selected_topics = []
+            for item in request.form.keys():
+                if item != 'done':
+                    ## put in the DB
+                    selected_topics.append(item)
+            return redirect(url_for('hello'))
+        else:
+            new_topic = request.form.get('search')
+            topics.append(new_topic)
+            selected.append(new_topic)
+            return render_template('wmyh.html', topics=topics, selected=selected)
 
-
-
+    return render_template('wmyh.html', topics=topics)
