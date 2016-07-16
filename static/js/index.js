cf247ee3939d45616276f51cfa3fd919a336749f
Kelly Cheng <iluvpippi@gmail.com>
2016-07-16 03:50:28 -0700

    Redirects to hello page when hit end of carousel

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+    var edgeFlag = false;
+    // On edge hit
+    $('.image-container').on('edge', function(event, slick, direction){
+      console.log('edge was hit');
+    });
+
+    // On before slide change
+    $('.image-container').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      if(nextSlide==14) {
+        edgeFlag = true;
+        console.log("hit end");
+      }
+    });
+
+
+
     $('.image-container').slick({
         dots: true,
         infinite: false,
@@ -10,5 +26,13 @@ $(document).ready(function(){
         centerPadding: '60px',
 
     });
+
+    $('.slick-next').click(function() {
+        console.log("click");
+        if (edgeFlag) {
+            window.location.href = "../hello";
+        }
+    });
+    
 });
 
